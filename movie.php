952ebbed154f0e85a9d6a64138ff863169709667
brianroysar <brian.sinambela1@gmail.com>
2021-10-24 19:32:08 -0700

    average rating for movie'

--- movie.php
+++ movie.php
@@ -4,7 +4,7 @@ provided as the id parameter of the URL.
 For example, the URL http://localhost:8888/movie.php?id=705 must display the information 
 on the movie “Charlies Angels.” This page must
 
-1. Show hyperlinks to the actor pages for each actor that was in this movie.
+1. Show hyperlinks to the actor pages for each actor that was in this movie. (DONE)
 2. Show the average score of the movie based on the user feedback.
 3. Show all user comments, including the reviewer’s name, rating, comments and the time it was provided.
 4. Contain an “add Comment” link/button, which links to the movie’s review page described below. */
@@ -40,6 +40,15 @@ while ($row = $rs->fetch_assoc()) {
 }
 
 print "<br>";
+
+// Showing the average score of the movie based on user review
+$query = "SELECT AVG(R.rating) as avg_rating FROM Movie as M, Review as R WHERE id ='".$movie_id."' and M.id = R.mid";
+$rs = $db->query($query);
+
+while ($row = $rs->fetch_assoc()) { 
+    $avg = $row['avg_rating']; 
+    print "average rating: $avg <br>";
+}
 
 // Showing the actors that are in the movie chosen
 print "Actors that are in the movie:";
