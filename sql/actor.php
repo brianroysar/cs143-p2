800dfd4eac4d277144896512185b2c9e6c5f0800
brianroysar <brian.sinambela1@gmail.com>
2021-10-24 18:00:43 -0700

    added .sql files from project 1

--- sql/actor.php
+++
@@ -1,1 +0,0 @@
-<h1>Actor Page</h1>
---
+++ sql/create.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Movie (
+    id int PRIMARY KEY,
+    title varchar(100),
+    year int,
+    rating varchar(10),
+    company varchar(50)
+);
+
+CREATE TABLE Actor (
+   id int PRIMARY KEY,
+   last varchar(20),
+   first varchar(20),
+   sex varchar(6),
+   dob DATE,
+   dod DATE
+);
+
+CREATE TABLE MovieGenre (
+    mid int,
+    genre varchar(20)
+);
+
+CREATE TABLE MovieActor (
+    mid int,
+    aid int, 
+    role varchar(50)
+);
+
+CREATE TABLE Review(
+    name varchar(20),
+    time DATETIME,
+    mid int,
+    rating int,
+    comment TEXT
+);
