0b9504e3c0639e6c288bf621e153921017c42373
brianroysar <brian.sinambela1@gmail.com>
2021-10-24 20:40:05 -0700

    added printing reviews

--- movie.php
+++ movie.php
@@ -5,7 +5,7 @@ For example, the URL http://localhost:8888/movie.php?id=705 must display the inf
 on the movie “Charlies Angels.” This page must
 
 1. Show hyperlinks to the actor pages for each actor that was in this movie. (DONE)
-2. Show the average score of the movie based on the user feedback.
+2. Show the average score of the movie based on the user feedback. (DONE)
 3. Show all user comments, including the reviewer’s name, rating, comments and the time it was provided.
 4. Contain an “add Comment” link/button, which links to the movie’s review page described below. */
 ?>
@@ -49,6 +49,21 @@ while ($row = $rs->fetch_assoc()) {
     $avg = $row['avg_rating']; 
     print "average rating: $avg <br>";
 }
+
+// Showing all user comments, including the reviewer’s name, rating, comments and the time it was provided.
+$query = "SELECT * FROM Review WHERE mid = '".$movie_id."'";
+$rs = $db->query($query);
+
+print "Reviews: <br>";
+
+while ($row = $rs->fetch_assoc()) { 
+    $name = $row['name']; 
+    $time = $row['time']; 
+    $rating = $row['rating'];
+    $comment = $row['comment'];
+    print "Reviewer Name: $name, Time of Review: $time, Rating: $rating, Comment: $comment<br>"; 
+}
+
 
 // Showing the actors that are in the movie chosen
 print "Actors that are in the movie:";
